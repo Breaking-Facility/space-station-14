d2f5a997173f275ccbda7bb13e66f016a689240c
Smirnov Peter <131467813+Sh18RW@users.noreply.github.com>
2024-09-08 18:39:08 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Добавление базового TTS с Corvax""""""""

---
+++ Content.Server/BF/TTS/TTSSystem.RateLimit.cs
@@ -0,0 +1,35 @@
+﻿using Content.Server.Chat.Managers;
+using Content.Server.Players.RateLimiting;
+using Content.Shared.BF.CCVars;
+using Robust.Shared.Player;
+
+namespace Content.Server.BF.TTS;
+
+public sealed partial class TTSSystem
+{
+    [Dependency] private readonly PlayerRateLimitManager _rateLimitManager = default!;
+    [Dependency] private readonly IChatManager _chat = default!;
+
+    private const string RateLimitKey = "TTS";
+
+    private void RegisterRateLimits()
+    {
+        _rateLimitManager.Register(RateLimitKey,
+            new RateLimitRegistration
+            {
+                CVarLimitPeriodLength = BfCCVars.TtsRateLimitPeriod,
+                CVarLimitCount = BfCCVars.TtsRateLimitCount,
+                PlayerLimitedAction = RateLimitPlayerLimited,
+            });
+    }
+
+    private void RateLimitPlayerLimited(ICommonSession player)
+    {
+        _chat.DispatchServerMessage(player, Loc.GetString("tts-rate-limited"), suppressLog: true);
+    }
+
+    private RateLimitStatus HandleRateLimit(ICommonSession player)
+    {
+        return _rateLimitManager.CountAction(player, RateLimitKey);
+    }
+}
--- Content.Server/VoiceMask/VoiceMaskSystem.Equip.cs
+++ Content.Server/VoiceMask/VoiceMaskSystem.Equip.cs
@@ -18,6 +18,10 @@ public sealed partial class VoiceMaskSystem
         var comp = EnsureComp<VoiceMaskComponent>(user);
         comp.VoiceName = component.LastSetName;
         comp.SpeechVerb = component.LastSpeechVerb;
+        // BF-TTS-Start
+        if (component.LastSetVoice != null)
+            comp.VoiceId = component.LastSetVoice;
+        // BF-TTS-End
 
         _actions.AddAction(user, ref component.ActionEntity, component.Action, uid);
     }
